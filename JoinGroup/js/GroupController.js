46cdb47201b12ad4d09b8d3eafc3c13710525ba9
Timo Stricker <TimoStricker@Timos-MacBook-Pro.local>
2015-10-26 20:33:29 -0600

    asd

--- JoinGroup/js/GroupController.js
+++ JoinGroup/js/GroupController.js
@@ -1,5 +1,14 @@
-angular.module('JMU', ['ngStamplay', 'ngRoute'])
-
-  .controller('GroupCtrl', function ($scope, $stamplay) {
-  
+angular.module('JMU', ['ngStamplay', 'ngRoute'], function ($locationProvider) {
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
   });
+})
+  .controller('GroupCtrl', function ($scope, $location) {
+  $scope.init = function () {
+    console.log("init function called");
+
+  };
+  $scope.target = $location.search()['tid'];
+
+});
