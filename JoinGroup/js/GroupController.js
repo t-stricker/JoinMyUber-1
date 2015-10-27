bd42db988b1c70ac415a48e1b62272db8e6650d6
Timo Stricker <TimoStricker@Timos-MacBook-Pro.local>
2015-10-26 21:27:19 -0600

    hurensohn

--- JoinGroup/js/GroupController.js
+++ JoinGroup/js/GroupController.js
@@ -4,21 +4,28 @@ angular.module('JMU', ['ngStamplay', 'ngRoute'], function ($locationProvider) {
     requireBase: false
   });
 })
-  .controller('GroupCtrl', function ($scope, $location) {
+  .controller('GroupCtrl', function ($scope, $stamplay, $location) {
   var user = $stamplay.User().Model;
+  $scope.userID;
 
   $scope.init = function () {
     console.log("init function called");
-
   };
+  
+  $scope.go = function () {
+    console.log("go");
+    $scope.userID = user.get('_id');
+  }
+  
+  
   $scope.id = $location.search()['id'];
-
   /* GET the current logged use data */
   user.currentUser()
     .then(function () {
     var userId = user.get('_id');
     var displayName = user.get('displayName');
-
+    
+  	
     if (userId) {
       console.log(userId);
       console.log(displayName);
